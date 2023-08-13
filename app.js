750e57fe70a54cffa8eaa5e3ed2b2c9e7e0b0af4
patakk <paolo.ceric@gmail.com>
2023-08-13 17:35:07 +0200

    good

--- app.js
+++ app.js
@@ -270,7 +270,7 @@ function handleEnd(event) {
     }
     mouseDown = false;
 
-    let rrn = (.05 + .05*(1-currntSat)*(1-currntVal)) * (-1 + 2 * Math.random());
+    let rrn = (.025 + .025*(1-currntSat)*(1-currntVal)) * (-1 + 2 * Math.random());
     currntHue = (pickedHue + rrn + 1.) % 1.;
     currntSat = pickedSat;
     currntVal = pickedVal;
