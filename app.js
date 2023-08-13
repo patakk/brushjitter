3639665a0e0bd590416e3552df55b0d13e080622
patakk <paolo.ceric@gmail.com>
2023-08-13 04:10:08 +0200

    good

--- app.js
+++ app.js
@@ -593,7 +593,7 @@ canvas.addEventListener('touchmove', function(event) {
             // Use deltaY to control your dragVal. 
             // Depending on your needs, you might scale the value or use it directly.
             dragVal = dragVal + deltaY * 0.03;  // scale the value for smoother control
-            pickedVal = max(0.0, min(1.0, dragVal));
+            pickedVal = Math.max(0.0, Math.min(1.0, dragVal));
             debugelement.innerHTML = "touchmove 22222 " + pickedVal + 'hh';
             initialTouchY = event.touches[0].clientY;
             initialTouchY2 = event.touches[1].clientY;
